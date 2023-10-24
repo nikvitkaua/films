8f1c68d6fd942c0f48a7e5838e7e0a9501f26712
Mykyta Paiuka <nikvitka.it@gmail.com>
2023-10-24 20:48:18 +0300

    code refactor

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -25,28 +11,27 @@ const movieDB = {
 };
 
 
-// #1
-document.querySelector('.promo__adv').remove();
+const adv = document.querySelectorAll('.promo__adv img');
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
 
-// #2
-document.querySelector('.promo__genre').innerHTML = "драма";
+adv.forEach(item => {
+    item.remove();
+});
 
-// #3
-document.querySelector('.promo__bg').style.background = "url(./img/bg.jpg) 0% 0% / cover no-repeat";
+genre.textContent = 'драма';
 
-// #4
-const moviesArr = movieDB.movies.sort();
-const filmList = document.querySelector('.promo__interactive-list');
-filmList.innerHTML = '';
+poster.style.backgroundImage = 'url(./img/bg.jpg)';
 
-for (let i = 0; i < moviesArr.length; i++) {
-    let listItem = document.createElement('li');
-    listItem.classList.add('promo__interactive-item');
+movieList.innerHTML = '';
 
-    listItem.innerHTML = `
-    ${i + 1}. ${moviesArr[i]}
-    <div class="delete"></div>
-    `
+movieDB.movies.sort();
 
-    filmList.append(listItem);
-}
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
